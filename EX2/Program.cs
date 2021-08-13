e0b05cef074e726007ff804038dc04ce38507460
Евгений Беккер <eug90@bk.ru>
2021-08-13 17:46:34 +0300

    Add project files.

---
+++ EX2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace EX2
+{
+    class Program
+    {
+        static class ArrayPair
+        {
+            static int count;
+
+            public static void SearchArrayPair(int[] arr)
+            {
+                Console.WriteLine("{0, 10}{1, 10}{2, 10}{3, 10}{4, 10}{5, 10}",
+                    "Pair 1", "Number 1", "Bool 1", "Pair 2", "Number 2", "Bool 2");
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if ((arr[i] % 3 == 0 && arr[i + 1] % 3 != 0)
+                       || (arr[i] % 3 != 0 && arr[i + 1] % 3 == 0))
+                    {
+                        count++;
+                        Console.WriteLine("{0, 10}{1, 10}{2, 10}{3, 10}{4, 10}{5, 10}",
+                            $"arr[{i}]", arr[i], arr[i] % 3 == 0, $"arr[{i + 1}]", arr[i + 1], arr[i + 1] % 3 == 0);
+                    }
+                }
+                Console.WriteLine($"\nPair count = {count}");
+            }
+            public static int[] SearchPairFromFile()
+            {
+                string fileName = "array.txt";
+                int[] arr = new int[20];
+                int index = 0;
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        string line;
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            arr[index] = int.Parse(line);
+                            index++;
+                        }
+                        sr.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The file could not be read!");
+                    Console.WriteLine(ex.Message);
+                }
+
+                return arr;
+            }
+            public static void ShowArray(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine($"arr[{i}] = {arr[i]}");
+                }
+            }
+        }
+        class Programe
+        {
+            static void Main(string[] args)
+            {
+                int[] arr = new int[20];
+                Random rnd = new Random();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(-10_000, 10_001);
+                }
+                Console.WriteLine("Пары массива, в которых только один делится на 3\n");
+                ArrayPair.SearchArrayPair(arr);
+
+               
+                int[] arr2 = ArrayPair.SearchPairFromFile();
+
+                Console.WriteLine("\nВывод массива из текстового файла\n");
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    Console.Write(arr2[i] + ", ");
+                }
+            }
+        }
+    }
+}
---
+++ Homework 4 Becker E.N/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework_4_Becker_E.N
+{
+  
+        class Program
+        {
+            static void Main()
+            {
+                int[] arrayInts = GetArrayWithRandomNum(20); //получить целочисленый массив
+                Console.WriteLine("Массив элементов из случайных чисел:");
+                Array.ForEach(arrayInts, Console.WriteLine);; //вывод всех элементов в консоль
+                Console.WriteLine();
+                int count = GetCountGoodNumbers(arrayInts); //получить количества пар
+                Console.WriteLine($"Количество пар элементов массива, в которых только одно число делится на 3 - {count}");
+             
+                
+            }
+           
+            static int[] GetArrayWithRandomNum(int size)
+            {
+                Random rnd = new Random();
+                int[] retArr = new int[size];
+                for (int i = 0; i < retArr.Length; i++)
+                {
+                    retArr[i] = rnd.Next(-10_000, 10_000);
+                }
+                return retArr;
+            }
+         
+            static int GetCountGoodNumbers(int[] arrayInts)
+            {
+                int count = default;
+                for (int i = 0; i < arrayInts.Length - 1; i++)
+                {
+                    if (arrayInts[i] % 3 == 0 ^ arrayInts[i + 1] % 3 == 0) //подсчет всех хороших пар чисел
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
